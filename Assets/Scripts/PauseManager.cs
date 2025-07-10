82daef8ca31a1b085e66e934978222955dcbaab1
EmilioeSe <emilio.michel@uniat.edu.mx>
2025-07-09 22:58:50 -0600

    Pause scrip and UI update
    Novedades
    -Added Folder "Images"
    -Remove Heart Container
    -Added "MainMenu" Scene to Scene List

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,28 +1,68 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPause : MonoBehaviour
 {
     public GameObject PauseMenu;
-    public bool Pause = false;
-    void Start()
-    {
-        
-    }
+    public GameObject SettingsMenu;
+    public AudioSource musicSource;  // Fuente de música de fondo
+    private bool isPaused = false;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Pause == false)
-            {
-                PauseMenu.SetActive(true);
-                Pause = true;
-
-                Time.timeScale = 0;
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
-            }
+            if (isPaused)
+                Resume();
+            else
+                Pause();
         }
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        SettingsMenu.SetActive(false);
+        Time.timeScale = 1f;
+        musicSource.UnPause();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+        Time.timeScale = 0f;
+        musicSource.Pause();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        isPaused = true;
+    }
+
+    public void OpenOptionsPanel()
+    {
+        PauseMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void OpenSettingsPanel()
+    {
+        PauseMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego");
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        // Asegúrate de que el menú principal esté en la escena 1
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
     }
 }
