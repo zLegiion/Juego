9197d26d6a79b4884a62a794197a5c3f1cdb5889
Von Valentine <winona.reyna@uniat.edu.mx>
2025-07-14 14:14:40 -0600

    fixing active video script

--- Assets/Scripts/EnemyDrop.cs
+++ Assets/Scripts/EnemyDrop.cs
@@ -6,9 +6,6 @@ public class EnemyDrop : MonoBehaviour
     [SerializeField] private GameObject itemPrefab;   
     [Range(0, 1)] public float percentProbability = 0.08f;   
 
-    [SerializeField] private GameObject activeVideo;
-    [SerializeField] private VideoPlayer video;
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
@@ -22,8 +19,6 @@ public class EnemyDrop : MonoBehaviour
         if (Random.value <= percentProbability)
         {
             GameObject itemGO = Instantiate(itemPrefab, transform.position, Quaternion.identity);
-            VideoPickUp item = itemGO.GetComponent<VideoPickUp>();
-            item.Init(activeVideo, video);
         }
     }
 }
