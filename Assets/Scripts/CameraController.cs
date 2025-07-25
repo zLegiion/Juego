64a50bfed74f63dc23617edd4abc98ee681e12d8
EmilioeSe <emilio.michel@uniat.edu.mx>
2025-07-25 09:22:55 -0600

    Camera Fix
    New
    -Camera no longer move on Y axis

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,11 +6,22 @@ public class CameraController : MonoBehaviour
     public float VelocidadCamara = 0.25f;
     public Vector3 desplazamiento;
 
+    private float yFija;
+
+    private void Start()
+    {
+        yFija = transform.position.y;
+    }
+
     private void LateUpdate()
     {
-        Vector3 posicionDeseada = Objetivo.position + desplazamiento;
-        Vector3 posicionSuavisada = Vector3.Lerp(transform.position, posicionDeseada, VelocidadCamara);
+        Vector3 posicionDeseada = new Vector3(
+            Objetivo.position.x + desplazamiento.x,
+            yFija,
+            Objetivo.position.z + desplazamiento.z
+        );
 
+        Vector3 posicionSuavisada = Vector3.Lerp(transform.position, posicionDeseada, VelocidadCamara);
         transform.position = posicionSuavisada;
     }
 }
